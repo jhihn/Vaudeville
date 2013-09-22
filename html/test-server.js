0669e0354a6b872eadec170eae8f940e49865eb9
Donald Mull Jr <donald.mull@gmail.com>
2013-09-22 15:30:03 -0400

    click event

--- html/test-server.js
+++ html/test-server.js
@@ -2,6 +2,12 @@ var express = require('express');
 
 app = express();
 
+// simple logger
+app.use(function(req, res, next){
+  console.log('%s %s', req.method, req.url);
+  next();
+});
+
 app.use(express.static(__dirname)); //serve static files from here.
 
 app.listen(8888);
